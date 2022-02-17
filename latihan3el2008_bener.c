4396cbe2400ec60b7a73170fb7e18c64527d4b9a
Fadiah Mumtaz Andevi <71587146+fadimtz@users.noreply.github.com>
2022-02-17 17:12:35 +0700

    Update latihan3el2008_bener.c
    3 - 17.12

--- latihan3el2008_bener.c
+++ latihan3el2008_bener.c
@@ -1,11 +1,11 @@
 /*EL2208 Praktikum Pemecahan Masalah dengan C 2021/2022
 *Modul            : Overview of C Language
-*Percobaan        : 
+*Percobaan        :3
 *Hari dan Tanggal : Kamis, 17 Februari 2022
 *Nama (NIM)       : 18320009
 *Asisten (NIM)    : 13219010
 *Nama File        : soal-01.c
-*Deskripsi        : -
+*Deskripsi        : membuatkan platform rekomendasi pilihan harga yang murah dan perjalanan yang singkat
 */
 
 #include <stdio.h>
@@ -13,7 +13,7 @@
 
 int main()
 {
-    int i;
+        int i;
     int n; /* banyaknya data */
     int x[100]; /* harga */
     int y[100]; /* lama perjalanan */
@@ -42,7 +42,7 @@ int main()
 
     for (i=0 ; i<n ; i++)
     {
-        if (x[i] > murah)
+        if (x[i] < murah)
         {
             murah = x[i];
             indeks_murah = i;
@@ -52,14 +52,41 @@ int main()
     int singkat = y[0]; /* inisialisasi untuk menentukan jarak paling dekat */
     int indeks_singkat;
 
-    for (i=0 ; i<n ; i++);
+    for (i=0 ; i<n ; i++)
     {
         if (y[i] < singkat)
         {
-            singkat = x[i];
+            singkat = y[i];
             indeks_singkat = i;
         }
-    }   
+    }
+
+    /* kalau harga murah ada yang sama, cari yg singkat */
+    for (i=0 ; i<n ; i++)
+    {
+        if (x[i] == murah)
+        {
+            if (y[i] < singkat)
+            {
+                singkat = y[i];
+                indeks_singkat = i;
+            }
+        }
+    }
+
+    /* kalau lama perjalanan ada yang sama, cari yg murah */
+    for (i=0 ; i<n ; i++)
+    {
+        if (y[i] == singkat)
+        {
+            if (x[i] < murah)
+            {
+                murah = x[i];
+                indeks_murah = i;
+            }
+        }
+    }
+
     if (menu == 1)
     {
         printf("Berikut adalah detail pilihan harga termurah yang disarankan:\n");
@@ -76,9 +103,14 @@ int main()
 
     if (menu == 3)
     {
-        
+        printf("Berikut adalah detail pilihan yang disarankan:\n");
+        printf("Harga termurah:\n");
+        printf("Harga = Rp %d\n", x[indeks_murah]);
+        printf("Lama perjalanan = %d Jam\n", y[indeks_murah]);
+        printf("Perjalanan tersingkat:\n");
+        printf("Harga = Rp %d\n", x[indeks_singkat]);
+        printf("Lama perjalanan = %d Jam\n", y[indeks_singkat]);
     }
-
 
     return 0;
 }
